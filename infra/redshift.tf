997c8e91d6aa46828598b234a110958c68ed67f7
SirWhizzy <dourbest.tolu@gmail.com>
2025-08-07 00:20:32 +0100

    New Update

--- infra/redshift.tf
+++ infra/redshift.tf
@@ -1,5 +1,13 @@
+
+#USERNAME
+resource "aws_ssm_parameter" "redshift_db_username" {
+  name  = "redshift_db_username"
+  type  = "String"
+  value = "Tolu"
+}
+
 # PASSWORD AND SECRETS
-resource "random_password" "redshift-password" {
+resource "random_password" "redshift_password" {
   length           = 16
   override_special = "()!#$%&_+-={}|" # A list of special characters that will be used in creating the password
   special          = true
@@ -7,54 +15,18 @@ resource "random_password" "redshift-password" {
   upper            = true
 }
 
-resource "aws_secretsmanager_secret" "redshift-admin-secrets" {
-  name                    = "randomdata/dev/redshift-admin-creds/"
-  description             = "Admin credentials for the redshift cluster"
-  recovery_window_in_days = 0
+resource "aws_ssm_parameter" "redshift_db_password" {
+  name  = "redshift_db_password"
+  type  = "String"
+  value = random_password.redshift_password.result
 }
 
-resource "aws_secretsmanager_secret_version" "redshift_admin_secrets_version" {
-  secret_id = aws_secretsmanager_secret.redshift-admin-secrets.id
-  secret_string = jsonencode({
-    username = "randomdata-redshift-admin"
-    password = random_password.redshift-password.result
-  })
-}
-
-
-# SECURITY GROUP CONFIG
-resource "aws_security_group" "redshift-security-group" {
-  name   = "randomdata-redshift-sg"
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "redshift-ingress-rule" {
-  security_group_id = aws_security_group.redshift-security-group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 5439
-  to_port           = 5439
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_egress_rule" "redshift-egress-rule" {
-  security_group_id = aws_security_group.redshift-security-group.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-
-# SUBNET GROUP
-resource "aws_redshift_subnet_group" "redshift-subnet-group" {
-  name       = "randomdata-dev-redshift-subnet-group"
-  subnet_ids = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
-}
-
-
-# CLUSTER
+# REDSHIFT CLUSTER
 resource "aws_redshift_cluster" "redshift_cluster" {
   cluster_identifier        = "randomdata-dev-redshift-cluster"
   database_name             = "random_data_db"
-  master_username           = "randomdata-redshift-admin"
-  master_password           = random_password.redshift-password.result
+  master_username           = aws_ssm_parameter.redshift_db_username.value
+  master_password           = aws_ssm_parameter.redshift_db_password.value
   node_type                 = "ra3.large"
   cluster_type              = "multi-node"
   number_of_nodes           = 2
@@ -63,5 +35,56 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   encrypted                 = true
   cluster_subnet_group_name = aws_redshift_subnet_group.redshift-subnet-group.name
   vpc_security_group_ids    = [aws_security_group.redshift-security-group.id]
-  skip_final_snapshot       = true
+  iam_roles                 = [aws_iam_role.redshift_role.arn]
+}
+
+
+#ROLES AND POLICY
+resource "aws_iam_role" "redshift_role" {
+  name = "redshift_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "redshift.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "redshift_policy" {
+  name = "redshift_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListAllBuckets",
+          "redshift:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "redshift_role_policy_attachment" {
+  name       = "redshift_role_policy_attachment"
+  roles      = [aws_iam_role.redshift_role.name]
+  policy_arn = aws_iam_policy.redshift_policy.arn
+}
+
+resource "aws_redshift_cluster_iam_roles" "redshift_cluster_iam_roles" {
+  cluster_identifier = aws_redshift_cluster.redshift_cluster.cluster_identifier
+  iam_role_arns      = [aws_iam_role.redshift_role.arn]
 }
